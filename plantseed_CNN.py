33e0047847d7810e23ccf5a4f1af841390365883
HaotianCheng <hcheng3@bu.edu>
2018-11-11 22:50:09 -0500

    sprint3 demo

--- plantseed_CNN.py
+++ plantseed_CNN.py
@@ -36,7 +36,7 @@ take_train_samples= True
 take_test_samples= True
 num_test_samples= 200
 
-show_plot=False
+show_plot = False
 
 ## read train and test data
 
@@ -156,18 +156,17 @@ def preprocess_image(img):
 
 target_image_size = 299
 
-# read, preprocess training and validation images
+# read, preprocess training and validation images  
 if not load_bf_train:
     x_train_valid = np.zeros((len(train_df), target_image_size, target_image_size, 3),
                              dtype='float32')
     y_train_valid = train_df.loc[:, 'species_id'].values
     for i, filepath in tqdm(enumerate(train_df['filepath'])):
-
         # read original images
-        #img = read_image(filepath, (target_image_size, target_image_size))
+        # img = read_image(filepath, (target_image_size, target_image_size))
 
         # read segmented image
-        _,_,_,img = read_segmented_image(filepath, (299, 299))
+        _, _, _, img = read_segmented_image(filepath, (299, 299))
 
         # all pixel values are now between -1 and 1
         x_train_valid[i] = preprocess_image(np.expand_dims(img.copy().astype(np.float), axis=0))
@@ -289,52 +288,181 @@ print('y_train.shape = ', y_train.shape)
 print('x_valid_bf.shape = ', x_valid_bf.shape)
 print('y_valid.shape = ', y_valid.shape)
 
-## logistic regression
 
-cv_num = 1
 
-acc_logreg_train = acc_logreg_valid = 0
-y_test_pred_proba_logreg = 0
+## neural network with tensorflow
 
-for i in range(cv_num):
+# permutation array for shuffling train data
+perm_array_train = np.arange(len(x_train_bf)) 
+index_in_epoch = 0
+
+# function: to get the next mini batch
+def get_next_batch(batch_size):
     
-    shuffle_train_valid_data() # shuffle data
+    global index_in_epoch, perm_array_train
+  
+    start = index_in_epoch
+    index_in_epoch += batch_size
     
-    logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
-    logreg.fit(x_train_bf, one_hot_to_dense(y_train))
-   
-    acc_logreg_train += logreg.score(x_train_bf, one_hot_to_dense(y_train))
-    acc_logreg_valid += logreg.score(x_valid_bf, one_hot_to_dense(y_valid))
-
-    y_test_pred_proba_logreg += logreg.predict_proba(x_test_bf)
+    if index_in_epoch > train_set_size:
+        np.random.shuffle(perm_array_train) # shuffle data
+        start = 0 # start next epoch
+        index_in_epoch = batch_size
+              
+    end = index_in_epoch
     
-acc_logreg_train /= cv_num
-acc_logreg_valid /= cv_num
-y_test_pred_proba_logreg /= cv_num
-y_test_pred_class_logreg = np.argmax(y_test_pred_proba_logreg, axis = 1)
+    return x_train_bf[perm_array_train[start:end]], y_train[perm_array_train[start:end]]
 
-print('Logistic Regression')
-print('Accuracy train/valid = %.4f/%.4f'%(acc_logreg_train, acc_logreg_valid))
-print('y_test_pred_class_logreg.shape = ', y_test_pred_class_logreg.shape)
+x_size = x_train_bf.shape[1] # number of features
+y_size = num_species # binary variable
+n_n_fc1 = 1024 # number of neurons of first layer
+n_n_fc2 = num_species # number of neurons of second layer
 
+# variables for input and output 
+x_data = tf.placeholder('float', shape=[None, x_size])
+y_data = tf.placeholder('float', shape=[None, y_size])
+
+# 1.layer: fully connected
+W_fc1 = tf.Variable(tf.truncated_normal(shape = [x_size, n_n_fc1], stddev = 0.1))
+b_fc1 = tf.Variable(tf.constant(0.1, shape = [n_n_fc1]))  
+h_fc1 = tf.nn.relu(tf.matmul(x_data, W_fc1) + b_fc1)
+
+# add dropout
+tf_keep_prob = tf.placeholder('float')
+h_fc1_drop = tf.nn.dropout(h_fc1, tf_keep_prob)
+
+# 3.layer: fully connected
+W_fc2 = tf.Variable(tf.truncated_normal(shape = [n_n_fc1, n_n_fc2], stddev = 0.1)) 
+b_fc2 = tf.Variable(tf.constant(0.1, shape = [n_n_fc2]))  
+z_pred = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+# cost function
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_data, 
+                                                                       logits=z_pred));
+
+# optimisation function
+tf_learn_rate = tf.placeholder(dtype='float', name="tf_learn_rate")
+train_step = tf.train.AdamOptimizer(tf_learn_rate).minimize(cross_entropy)
+
+# evaluation
+y_pred = tf.cast(tf.nn.softmax(z_pred), dtype = tf.float32);
+y_pred_class = tf.cast(tf.argmax(y_pred,1), tf.int32)
+y_data_class = tf.cast(tf.argmax(y_data,1), tf.int32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(y_pred_class, y_data_class), tf.float32))
+
+# parameters
+cv_num = 1 # number of cross validations
+n_epoch = 15 # number of epochs
+batch_size = 50 
+keep_prob = 0.33 # dropout regularization with keeping probability
+learn_rate_range = [0.01,0.005,0.0025,0.001,0.001,0.001,0.00075,0.0005,0.00025,0.0001,
+                   0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001,0.0001];
+learn_rate_step = 3 # in terms of epochs
+
+acc_train_DNN = 0
+acc_valid_DNN = 0
+loss_train_DNN = 0
+loss_valid_DNN = 0
+y_test_pred_proba_DNN = 0
+y_valid_pred_proba = 0
+
+# use cross validation
+for j in range(cv_num):
+    
+    # start TensorFlow session and initialize global variables
+    sess = tf.InteractiveSession() 
+    sess.run(tf.global_variables_initializer())  
+
+    # shuffle train/validation splits
+    shuffle_train_valid_data() 
+    n_step = -1;
+
+    # training model
+    for i in range(int(n_epoch*train_set_size/batch_size)):
+
+        if i%int(learn_rate_step*train_set_size/batch_size) == 0:
+            n_step += 1;
+            learn_rate = learn_rate_range[n_step];
+            print('learnrate = ', learn_rate)
+        
+        x_batch, y_batch = get_next_batch(batch_size)
+        
+        sess.run(train_step, feed_dict={x_data: x_batch, y_data: y_batch, 
+                                        tf_keep_prob: keep_prob, 
+                                        tf_learn_rate: learn_rate})
+
+        if i%int(0.25*train_set_size/batch_size) == 0:
+            
+            train_loss = sess.run(cross_entropy,
+                                  feed_dict={x_data: x_train_bf[:valid_set_size], 
+                                             y_data: y_train[:valid_set_size], 
+                                             tf_keep_prob: 1.0})
+
+            
+            train_acc = accuracy.eval(feed_dict={x_data: x_train_bf[:valid_set_size], 
+                                                 y_data: y_train[:valid_set_size], 
+                                                 tf_keep_prob: 1.0})    
+
+            valid_loss = sess.run(cross_entropy,feed_dict={x_data: x_valid_bf, 
+                                                           y_data: y_valid, 
+                                                           tf_keep_prob: 1.0})
+
+           
+            valid_acc = accuracy.eval(feed_dict={x_data: x_valid_bf, 
+                                                 y_data: y_valid, 
+                                                 tf_keep_prob: 1.0})      
+
+            print('%.2f epoch: train/val loss = %.4f/%.4f, train/val acc = %.4f/%.4f'%(
+                (i+1)*batch_size/train_set_size, train_loss, valid_loss, train_acc, valid_acc))
+
+    
+    acc_train_DNN += train_acc
+    acc_valid_DNN += valid_acc
+    loss_train_DNN += train_loss
+    loss_valid_DNN += valid_loss
+    
+    y_valid_pred_proba += y_pred.eval(feed_dict={x_data: x_valid_bf, tf_keep_prob: 1.0}) 
+    y_test_pred_proba_DNN += y_pred.eval(feed_dict={x_data: x_test_bf, tf_keep_prob: 1.0}) 
+
+    sess.close()
+        
+acc_train_DNN /= float(cv_num)
+acc_valid_DNN /= float(cv_num)
+loss_train_DNN /= float(cv_num)
+loss_valid_DNN /= float(cv_num)
+
+# final validation prediction
+y_valid_pred_proba /= float(cv_num)
+y_valid_pred_class = np.argmax(y_valid_pred_proba, axis = 1)
+
+# final test prediction
+y_test_pred_proba_DNN /= float(cv_num)
+y_test_pred_class_DNN = np.argmax(y_test_pred_proba_DNN, axis = 1)
+
+# final loss and accuracy
+print('')
+print('final: train/val loss = %.4f/%.4f, train/val acc = %.4f/%.4f'%(loss_train_DNN, 
+                                                                      loss_valid_DNN, 
+                                                                      acc_train_DNN, 
+                                                                      acc_valid_DNN))
 
 ## show confusion matrix
+    
+cnf_matrix = confusion_matrix(one_hot_to_dense(y_valid), y_valid_pred_class)
 
-# cnf_matrix = confusion_matrix(one_hot_to_dense(y_valid), y_valid_pred_class)
-#
-# abbreviation = ['BG', 'Ch', 'Cl', 'CC', 'CW', 'FH', 'LSB', 'M', 'SM', 'SP', 'SFC', 'SB']
-# pd.DataFrame({'class': species, 'abbreviation': abbreviation})
-#
-# fig, ax = plt.subplots(1)
-# ax = sns.heatmap(cnf_matrix, ax=ax, cmap=plt.cm.Greens, annot=True)
-# ax.set_xticklabels(abbreviation)
-# ax.set_yticklabels(abbreviation)
-# plt.title('Confusion matrix of validation set')
-# plt.ylabel('True species')
-# plt.xlabel('Predicted species')
-# plt.show()
+abbreviation = ['BG', 'Ch', 'Cl', 'CC', 'CW', 'FH', 'LSB', 'M', 'SM', 'SP', 'SFC', 'SB']
+pd.DataFrame({'class': species, 'abbreviation': abbreviation})
 
-y_test_pred_class = y_test_pred_class_logreg
+fig, ax = plt.subplots(1)
+ax = sns.heatmap(cnf_matrix, ax=ax, cmap=plt.cm.Greens, annot=True)
+ax.set_xticklabels(abbreviation)
+ax.set_yticklabels(abbreviation)
+plt.title('Confusion matrix of validation set')
+plt.ylabel('True species')
+plt.xlabel('Predicted species')
+plt.show()
+
+y_test_pred_class = y_test_pred_class_DNN
 test_df['species_id'] = y_test_pred_class
 test_df['species'] = [species[sp] for sp in y_test_pred_class]
 test_df[['file', 'species']].to_csv('submission.csv', index=False)
